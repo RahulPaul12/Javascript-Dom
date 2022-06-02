14a4b5cb8f7aad4d13b2850a7bca91d740da1e83
Rahul Paul <rahulpaul199914@gmail.com>
2022-06-03 01:00:35 +0600

    Add files via upload

---
+++ color-changer with hexacode/app.js
@@ -0,0 +1,24 @@
+window.onload=()=>{
+   main();
+}
+
+function main(){
+    let body= document.getElementById('root');
+    let btn = document.getElementById('btn');
+    let output = document.getElementById('output');
+    let para = document.getElementById('para');
+    btn.addEventListener('click',function(){
+        let bgcolor = getbgcolor();
+        body.style.backgroundColor=bgcolor;
+        output.value= bgcolor;
+       
+    })
+}
+
+function getbgcolor(){
+    const red = Math.floor(Math.random() * 255);
+	const green = Math.floor(Math.random() * 255);
+	const blue = Math.floor(Math.random() * 255);
+    
+	return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+}
